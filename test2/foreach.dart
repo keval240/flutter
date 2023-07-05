fb2f8020f9846bcfc9aef7961a7671c61a5ac765
keval240 <137788642+keval240@users.noreply.github.com>
2023-07-05 13:15:45 +0530

    Add files via upload

---
+++ test2/foreach.dart
@@ -0,0 +1,6 @@
+void main()
+{
+    var data=['a','b','c','d','1'];
+
+    data.forEach((var a)=>print(a));
+}
---
+++ test2/number_check.dart
@@ -0,0 +1,21 @@
+void main()
+{
+   int a=-1;
+
+
+
+   if(a>0)
+   {
+     print('number is positive');
+   }
+
+   else if(a<0)
+   {
+     print('number is negative');
+
+   }
+   else
+     {
+       print('number is 0');
+     }
+}
